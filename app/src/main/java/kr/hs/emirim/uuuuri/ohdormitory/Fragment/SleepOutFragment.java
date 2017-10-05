fc39cc2a3306a40a192a5cfc1030a522b3733d9e
zoripong <dbfl0406@e-mirim.hs.kr>
2017-10-05 08:15:05 -0300

    exception in qrcam xml

--- app/src/main/java/kr/hs/emirim/uuuuri/ohdormitory/Fragment/SleepOutFragment.java
+++ app/src/main/java/kr/hs/emirim/uuuuri/ohdormitory/Fragment/SleepOutFragment.java
@@ -1,12 +1,15 @@
 package kr.hs.emirim.uuuuri.ohdormitory.Fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import kr.hs.emirim.uuuuri.ohdormitory.Activity.QRCamActivity;
 import kr.hs.emirim.uuuuri.ohdormitory.R;
 
 /**
@@ -15,8 +18,22 @@ import kr.hs.emirim.uuuuri.ohdormitory.R;
 
 public class SleepOutFragment extends Fragment {
 
+    Button mCameraBtn;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.sleep_out_fragment, container, false);
+
+        View view=inflater.inflate(R.layout.sleep_out_fragment, container, false);
+
+        view.findViewById(R.id.camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), QRCamActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
+
     }
 }
